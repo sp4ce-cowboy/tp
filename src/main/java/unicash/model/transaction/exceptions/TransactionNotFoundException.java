9534aedff1f5f9b04229f1b2ae303d8aa76b32e2
Rubesh S <rubesh77@gmail.com>
2023-10-30 16:23:51 +0800

    Add ability to stack filters

--- src/main/java/unicash/model/transaction/exceptions/TransactionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package unicash.model.transaction.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified transaction.
- */
-public class TransactionNotFoundException extends RuntimeException {
-
-}
