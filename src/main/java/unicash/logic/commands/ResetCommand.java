b0b109ada6ec77105522c9231f95feae0e9bef69
Rubesh S <rubesh77@gmail.com>
2023-11-13 05:05:59 +0800

    Fix bug in Reset Command

--- src/main/java/unicash/logic/commands/ResetCommand.java
+++ src/main/java/unicash/logic/commands/ResetCommand.java
@@ -1,15 +1,19 @@
 package unicash.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static unicash.model.Model.PREDICATE_SHOW_ALL_TRANSACTIONS;
 import static unicash.model.util.SampleDataUtil.getSampleUniCash;
 
 import unicash.commons.enums.CommandType;
 import unicash.commons.util.ToStringBuilder;
 import unicash.model.Model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Resets UniCa$h to initial state by replacing the current UniCa$h
- * storage data and populating it with the default UniCa$h containing
+ * Resets UniCash to initial state by replacing the current UniCash
+ * storage data and populating it with the default UniCash containing
  * typical transactions from {@code SampleDataUtil}.
  */
 public class ResetCommand extends Command {
@@ -20,11 +24,15 @@ public class ResetCommand extends Command {
 
     public static final String MESSAGE_FAILURE = CommandType.RESET.getMessageFailure();
 
+    private static final Logger logger = Logger.getLogger("ResetCommandLogger");
+
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setUniCash(getSampleUniCash());
+        model.updateFilteredTransactionList(PREDICATE_SHOW_ALL_TRANSACTIONS);
+        logger.log(Level.INFO, "Successfully reset UniCash!");
         return new CommandResult(MESSAGE_SUCCESS);
     }
 
