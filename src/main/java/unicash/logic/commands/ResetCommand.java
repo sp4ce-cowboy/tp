f533fcd4d1c7135cce45742db2f8f24d673b7d39
Rubesh S <rubesh77@gmail.com>
2023-11-13 05:08:09 +0800

    Fix style

--- src/main/java/unicash/logic/commands/ResetCommand.java
+++ src/main/java/unicash/logic/commands/ResetCommand.java
@@ -4,12 +4,13 @@ import static java.util.Objects.requireNonNull;
 import static unicash.model.Model.PREDICATE_SHOW_ALL_TRANSACTIONS;
 import static unicash.model.util.SampleDataUtil.getSampleUniCash;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import unicash.commons.enums.CommandType;
 import unicash.commons.util.ToStringBuilder;
 import unicash.model.Model;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Resets UniCash to initial state by replacing the current UniCash
